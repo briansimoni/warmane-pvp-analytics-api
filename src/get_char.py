89d04d2e08c5e71df1f930326243d18346cb7c79
Brian Simoni <brsimoni@gmail.com>
2022-08-14 10:49:29 -0400

    remove a header

--- src/get_char.py
+++ src/get_char.py
@@ -14,7 +14,6 @@ def get_char(event, context):
         print("got the metadata", metadata)
         return {
             "headers": {
-                "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*",
             },
             "statusCode": 200,
