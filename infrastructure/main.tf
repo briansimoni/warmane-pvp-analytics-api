8ab091b1f77258048b93f19a9d2dea0aa1c0ae44
Brian Simoni <brsimoni@gmail.com>
2023-03-06 00:16:20 +0000

    add some terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    cloud {
+        organization = "simoni-enterprises"
+        workspaces {
+            name = "warmane-pvp-analytics-api"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.57"
+        }
+    }
+}
+
+locals {
+    openapi_spec = yamldecode("./openapi.yaml")
+}
+
+
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name = "warmane_pvp_analytics_v2_api"
+  body = yamlencode(local.openapi_spec)
+}
