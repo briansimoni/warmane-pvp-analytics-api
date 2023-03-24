62fb9cbba1b546c90dc1d078fae555e0b7ce94b6
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-03-23 22:51:35 -0400

    -added Cheerio HTML Parser as dependency -removed "@types/axios" from devDependencies
    -getMatchIds()
    -added html? as optional parameter, allowing HTML to be injected from within for testing purposes
      -in normal operation, function will pull data from axios call
        * test file must be neutralized--if test file is present it will be used
      -when testing, function will pull data from test-file
    -added Cheerio html parser
      -returns 'matchIds' array, containg all match IDs from the given input

--- src/lib/crawler/crawler.test.ts
+++ src/lib/crawler/crawler.test.ts
@@ -1,24 +1,30 @@
-import axios from "axios";
+import { WarmaneCrawler } from "./crawler";
 import * as fs from "fs";
 import * as path from "path";
-import { WarmaneCrawler } from "./crawler";
-
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("crawler tests", () => {
-  const testFile = path.join(__dirname, "test.html");
-  const html = String(fs.readFileSync(testFile));
-
+  // creating test case
   test("when an html document is returned with matches, it can return a list of match IDs", async () => {
-    mockedAxios.get.mockImplementation(async () => html);
+    // storing content of 'test.html' as string in testHtml
+    const testHtml = fs.readFileSync(
+      path.resolve(__dirname, "test.html"),
+      "utf-8"
+    );
+
+    // creating new instance of WarmaneCrawler
     const crawler = new WarmaneCrawler();
+    // calling 'getMatchIds' on 'crawler' with testHtml as a paramater
     const matchIds = await crawler.getMatchIds({
-      character: "Dumpster",
+      character: "SomeCharacter",
       realm: "Blackrock",
+      html: testHtml,
     });
+
+    // length of 'matchIds' array must be 453 to pass test
     expect(matchIds.length).toBe(453);
+
     expect(matchIds).toEqual(
+      // 'matchIds' array must contain specified IDs to pass
       expect.arrayContaining(["25309337", "25309969", "25314478"])
     );
   });
--- src/lib/crawler/crawler.ts
+++ src/lib/crawler/crawler.ts
@@ -1,19 +1,47 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
+// defining Crawler interface
 export interface Crawler {
   getMatchIds(params: { character: string; realm: string }): Promise<string[]>;
-  getMatchDetails(params: { matchIds: string[] }): Promise<any[]>;
+  getMatchDetails(params: { matchIds: string[] }): Promise<any[]>; //
 }
-
+// implementing the Crawler interface in the WarmaneCrawler class
 export class WarmaneCrawler implements Crawler {
+  // defining 'getMatchIds' method with optional paramater for testing (html)
   async getMatchIds(params: {
     character: string;
     realm: string;
+    // added optional paramater (HTML passed as string) for testing
+    html?: string;
   }): Promise<string[]> {
-    // throw new Error("Method not implemented.");
-    return ["1", "2", "3"];
+    // getMatchId will use optional parameter if present, will only use URL in its absence
+    const html =
+      params.html ??
+      (await axios
+        .get(
+          `https://armory.warmane.com/character/${params.character}/${params.realm}/match-history`
+        )
+        .then((response) => response.data));
+
+    // loading fetched HTML into Cheerio object
+    const $ = cheerio.load(html);
+    // initializing empty array (matchIds) to store match IDs
+    const matchIds: string[] = [];
+
+    // iterating through each table row with an ID of 'data-table-history'
+    //  - _index not used in callback, but needed as argument (index of current element, current element)
+    $("table#data-table-history tbody tr").each((_index, element) => {
+      // extracting 1st table cell's text, trimming whitespace, adding to 'matchIds' array
+      const matchId = $(element).find("td:first-child").text().trim();
+      // adding extracted ID to array
+      matchIds.push(matchId);
+    });
+    // returning updated array
+    return matchIds;
   }
 
+  // todo: implement 'getMatchDetails' method
   async getMatchDetails(params: { matchIds: string[] }): Promise<any[]> {
     throw new Error("Method not implemented.");
   }
