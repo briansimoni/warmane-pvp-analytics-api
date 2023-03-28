da3368b8b5753103a340c9bb980a101aed15a463
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-03-28 19:41:45 -0400

    Implement getMatchIds() (#3)
    * -added Cheerio HTML Parser as dependency
    -removed "@types/axios" from devDependencies
    
    -getMatchIds()
    -added html? as optional parameter, allowing HTML to be injected from within for testing purposes
      -in normal operation, function will pull data from axios call
        * test file must be neutralized--if test file is present it will be used
      -when testing, function will pull data from test-file
    -added Cheerio html parser
      -returns 'matchIds' array, containg all match IDs from the given input
    
    * crawler.ts
    -Removed optional 'html' variable
    -Replaced line with nullish coalescing operator to seperate lines that will fetch HTML w/ axios and extract content
    
    crawler.test.ts
    -imported axios
    -created a mock axios
    -updated test to mock axios implementation (returns object w/ data prop containing 'testHtml' string instead of directly returning 'testHtml' string)
    -removed 'html' parameter when calling getMatchIds() as no longer necessary

--- src/lib/crawler/crawler.test.ts
+++ src/lib/crawler/crawler.test.ts
@@ -1,22 +1,29 @@
-import axios from "axios";
+import { WarmaneCrawler } from "./crawler";
 import * as fs from "fs";
 import * as path from "path";
-import { WarmaneCrawler } from "./crawler";
+import axios from "axios";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("crawler tests", () => {
-  const testFile = path.join(__dirname, "test.html");
-  const html = String(fs.readFileSync(testFile));
-
   test("when an html document is returned with matches, it can return a list of match IDs", async () => {
-    mockedAxios.get.mockImplementation(async () => html);
+    // storing data from test.html file as string
+    const testHtml = fs.readFileSync(
+      path.resolve(__dirname, "test.html"),
+      "utf-8"
+    );
+
+    mockedAxios.get.mockImplementation(async () => ({
+      data: testHtml,
+    }));
+
     const crawler = new WarmaneCrawler();
     const matchIds = await crawler.getMatchIds({
-      character: "Dumpster",
+      character: "SomeCharacter",
       realm: "Blackrock",
     });
+
     expect(matchIds.length).toBe(453);
     expect(matchIds).toEqual(
       expect.arrayContaining(["25309337", "25309969", "25314478"])
--- src/lib/crawler/crawler.ts
+++ src/lib/crawler/crawler.ts
@@ -1,20 +1,38 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 
+// defining Crawler interface
 export interface Crawler {
   getMatchIds(params: { character: string; realm: string }): Promise<string[]>;
   getMatchDetails(params: { matchIds: string[] }): Promise<any[]>;
 }
 
+// implementing the Crawler interface in the WarmaneCrawler class
 export class WarmaneCrawler implements Crawler {
   async getMatchIds(params: {
     character: string;
     realm: string;
   }): Promise<string[]> {
-    // throw new Error("Method not implemented.");
-    return ["1", "2", "3"];
+    // Fetching the HTML using axios
+    const response = await axios.get(
+      `https://armory.warmane.com/character/${params.character}/${params.realm}/match-history`
+    );
+    const html: string = response.data;
+
+    // loading fetched HTML into Cheerio object
+    const $ = cheerio.load(html);
+    const matchIds: string[] = [];
+
+    $("table#data-table-history tbody tr").each((_index, element) => {
+      const matchId = $(element).find("td:first-child").text().trim();
+      matchIds.push(matchId);
+    });
+
+    return matchIds;
   }
 
   async getMatchDetails(params: { matchIds: string[] }): Promise<any[]> {
     throw new Error("Method not implemented.");
   }
 }
+
