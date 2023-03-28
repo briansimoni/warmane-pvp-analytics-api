6cf3bd9bbd02a8f19757a00338597bf023ccfa38
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-03-28 16:36:02 -0400

    crawler.ts -Removed optional 'html' variable -Replaced line with nullish coalescing operator to seperate lines that will fetch HTML w/ axios and extract content
    crawler.test.ts
    -imported axios
    -created a mock axios
    -updated test to mock axios implementation (returns object w/ data prop containing 'testHtml' string instead of directly returning 'testHtml' string)
    -removed 'html' parameter when calling getMatchIds() as no longer necessary

--- src/lib/crawler/crawler.test.ts
+++ src/lib/crawler/crawler.test.ts
@@ -1,30 +1,31 @@
 import { WarmaneCrawler } from "./crawler";
 import * as fs from "fs";
 import * as path from "path";
+import axios from "axios";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("crawler tests", () => {
-  // creating test case
   test("when an html document is returned with matches, it can return a list of match IDs", async () => {
-    // storing content of 'test.html' as string in testHtml
+    // storing data from test.html file as string
     const testHtml = fs.readFileSync(
       path.resolve(__dirname, "test.html"),
       "utf-8"
     );
 
-    // creating new instance of WarmaneCrawler
+    mockedAxios.get.mockImplementation(async () => ({
+      data: testHtml,
+    }));
+
     const crawler = new WarmaneCrawler();
-    // calling 'getMatchIds' on 'crawler' with testHtml as a paramater
     const matchIds = await crawler.getMatchIds({
       character: "SomeCharacter",
       realm: "Blackrock",
-      html: testHtml,
     });
 
-    // length of 'matchIds' array must be 453 to pass test
     expect(matchIds.length).toBe(453);
-
     expect(matchIds).toEqual(
-      // 'matchIds' array must contain specified IDs to pass
       expect.arrayContaining(["25309337", "25309969", "25314478"])
     );
   });
