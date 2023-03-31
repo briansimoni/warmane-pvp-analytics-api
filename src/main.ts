ec4e7c33df847578ee01a9c3176913c8019b80e8
Brian Simoni <brian.simoni@aura.com>
2023-03-31 12:55:57 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { APIGatewayEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import express from "express";
 
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
+  const app = express();
+  console.log(app);
   return {
     statusCode: 200,
     body: JSON.stringify({
