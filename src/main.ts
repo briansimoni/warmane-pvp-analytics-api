cb365ce866190ad1d289e1780495ff1b14c233f5
Brian Simoni <brsimoni@gmail.com>
2023-04-02 11:46:11 -0400

    Update api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { APIGatewayEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import axios from "axios";
 import express from "express";
 
 export const handler = async (
@@ -7,6 +8,13 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   const app = express();
   console.log(app);
+
+  if (
+    event.httpMethod.toLocaleLowerCase() === "get" &&
+    event.path.includes("character")
+  ) {
+    return await getCharacter(event, context);
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -14,3 +22,36 @@ export const handler = async (
     }),
   };
 };
+
+async function getCharacter(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  if (!event.pathParameters) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "bad request",
+      }),
+    };
+  }
+
+  const character = event.pathParameters["character"];
+  const realm = event.pathParameters["realm"];
+  if (!character || !realm) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "bad request",
+      }),
+    };
+  }
+  const response = await axios.get(
+    `http://armory.warmane.com/api/character/${character}/${realm}/profile`
+  );
+
+  return {
+    statusCode: 200,
+    body: response.data,
+  };
+}
