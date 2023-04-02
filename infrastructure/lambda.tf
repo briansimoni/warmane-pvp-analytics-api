2e3febaf867adee046c76a8c19bab16d103e916f
Brian Simoni <brsimoni@gmail.com>
2023-04-01 20:19:48 -0400

    add permission for lambda

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -26,6 +26,17 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.warmane_analytics_api_v2_main_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
+
 resource "aws_lambda_function" "warmane_analytics_api_v2_main_function" {
   function_name = "warmane_analytics_api_v2_main_function"
 
