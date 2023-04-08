07cc6e8ec355d82cf65aceda44b118a7f5b884b9
Brian Simoni <brsimoni@gmail.com>
2023-04-08 19:38:48 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,43 @@
 import { APIGatewayEvent, Context, APIGatewayProxyResult } from "aws-lambda";
 import axios from "axios";
-import express from "express";
 
-export const handler = async (
-  event: APIGatewayEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> => {
-  console.log("invoking the handler!");
-  console.log(event);
-  if (
-    event.httpMethod.toLocaleLowerCase() === "get" &&
-    event.path.includes("character")
-  ) {
-    try {
-      const result = await getCharacter(event, context);
-      console.log(result);
-      return result;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World",
-    }),
-  };
-};
+import Koa from "koa";
+import serverless from "aws-serverless-koa";
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = "hello world";
+});
+
+export const lambdaHandler = serverless(app);
+
+// export const handler = async (
+//   event: APIGatewayEvent,
+//   context: Context
+// ): Promise<APIGatewayProxyResult> => {
+//   console.log("invoking the handler!");
+//   console.log(event);
+//   if (
+//     event.httpMethod.toLocaleLowerCase() === "get" &&
+//     event.path.includes("character")
+//   ) {
+//     try {
+//       const result = await getCharacter(event, context);
+//       console.log(result);
+//       return result;
+//     } catch (err) {
+//       console.log(err);
+//       throw err;
+//     }
+//   }
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Hello World",
+//     }),
+//   };
+// };
 
 async function getCharacter(
   event: APIGatewayEvent,
