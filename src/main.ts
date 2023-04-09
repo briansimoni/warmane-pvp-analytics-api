13ce7785f2f6c076295a0e1fb4cac165c6870114
Brian Simoni <brsimoni@gmail.com>
2023-04-08 22:41:44 -0400

    more debug

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ app.use(AwsMiddleware({}));
 
 app.use(router.routes());
 
-if (process.env.AWS_EXECUTION_ENV === undefined) {
-  app.listen(3000, () => {
-    console.log("server listening on 3000");
-  });
-}
+// if (process.env.AWS_EXECUTION_ENV === undefined) {
+//   app.listen(3000, () => {
+//     console.log("server listening on 3000");
+//   });
+// }
 
 export const handler = serverless(app);
