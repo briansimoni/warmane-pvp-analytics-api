a5e8c80c704b4659422124aff9225baae465e194
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-04-17 20:22:47 -0400

    PR Review Corrections
    - removed unnecessary comments, refined remainder
    - added line breaks to all comments (no longer blinded by 'word-wrap: on')
    - removed debug logs

--- src/lib/crawler/crawler.ts
+++ src/lib/crawler/crawler.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import Bottleneck from "bottleneck";
 
-// provided by initial GET call w/ character name & realm
 export interface MatchSummary {
   matchId: string;
   team_name: string;
@@ -14,7 +13,6 @@ export interface MatchSummary {
   arena: string;
 }
 
-// provided by secondary POST call w/ match ID
 export interface CharacterDetail {
   realm: string;
   charname: string;
@@ -31,7 +29,11 @@ export interface CharacterDetail {
   personal_change: string;
 }
 
-// MatchSummary & CharacterDetail merged
+/**
+ * MatchDetails is an aggregate of all data that the crawler collects
+ * for a given matchId. These objects are intended to be stored
+ * long-term in some kind of database for further use and analysis.
+ */
 export interface MatchDetails {
   matchId: string;
   team_name: string;
@@ -42,10 +44,8 @@ export interface MatchDetails {
   duration: string;
   arena: string;
   character_details: CharacterDetail[];
-  // team?: string; // does team_name make this superfluous?
 }
 
-// defining Crawler interface
 export interface Crawler {
   getMatchSummaries(params: {
     character: string;
@@ -58,9 +58,8 @@ export interface Crawler {
   }): Promise<MatchDetails[]>;
 }
 
-// exporting crawler
 export class WarmaneCrawler implements Crawler {
-  // fetches match history HTML w/ GET request, returns HTML as string
+  // Fetches match history HTML w/ GET request, returns as string
   async fetchMatchHistoryHTML(params: {
     character: string;
     realm: string;
@@ -71,7 +70,7 @@ export class WarmaneCrawler implements Crawler {
     return response.data;
   }
 
-  // parses fetched HTML w/ Cheerio, makes array of MatchSummary objects with extracted info
+  // Extracts match summaries from match history HTML
   extractMatchSummaries(html: string): MatchSummary[] {
     const $ = cheerio.load(html);
     const matchSummaries: MatchSummary[] = [];
@@ -79,7 +78,7 @@ export class WarmaneCrawler implements Crawler {
     $("table#data-table-history tbody tr").each((_index, element) => {
       const matchId = $(element).find("td:nth-child(1)").text().trim();
       const outcome = $(element).find("td:nth-child(3)").text().trim();
-      const points_change = $(element).find("td:nth-child(4)").text().trim(); //? todo?
+      const points_change = $(element).find("td:nth-child(4)").text().trim();
       const date = $(element).find("td:nth-child(5)").text().trim();
       const duration = $(element).find("td:nth-child(6)").text().trim();
       const arena = $(element).find("td:nth-child(7)").text().trim();
@@ -99,7 +98,6 @@ export class WarmaneCrawler implements Crawler {
           bracket = teamBracketMatch[2];
         }
       }
-      // creates object with extracted match data
       const matchSummary: MatchSummary = {
         matchId,
         team_name,
@@ -110,14 +108,14 @@ export class WarmaneCrawler implements Crawler {
         outcome,
         duration,
       };
-      // pushes each match's object to array
+
       matchSummaries.push(matchSummary);
     });
 
     return matchSummaries;
   }
 
-  // calls fetchMatchHistoryHTML() & extractMatchSummaries()
+  // Retrieves match summaries for specified character and realm
   async getMatchSummaries(params: {
     character: string;
     realm: string;
@@ -127,7 +125,7 @@ export class WarmaneCrawler implements Crawler {
     return matchSummaries;
   }
 
-  // fetches match data for a given match ID, returns data as object
+  // Fetches match data for given match ID, returns array of CharacterDetail objects
   async fetchMatchData(matchId: string): Promise<CharacterDetail[]> {
     const response = await axios.post(
       "https://armory.warmane.com/character/Dumpster/Blackrock/match-history",
@@ -144,7 +142,12 @@ export class WarmaneCrawler implements Crawler {
     return response.data;
   }
 
-  // responsible for fetching match details using the match IDs of each given matchSummaries array, combining the details to create a single object, and restricting concurrent operations to 32
+  /**
+   * Fetches match details using match IDs of each given matchSummaries array,
+   * combines matchDetails objects with  corresponding matchSummary object,
+   * and restricts concurrent operations to 32.
+   */
+
   async getMatchDetails(params: {
     character: string;
     realm: string;
@@ -152,28 +155,22 @@ export class WarmaneCrawler implements Crawler {
   }): Promise<MatchDetails[]> {
     // extracts match IDs from 'matchSummaries' array
     const matchIds = params.matchSummaries.map((summary) => summary.matchId);
-    // creates empty array for 'matchDetails' objects
     const matchDetailsList: MatchDetails[] = [];
 
-    // initializes new Bottleneck instance to control concurrency
+    // initializes and implements new Bottleneck instance to control concurrency
     const limiter = new Bottleneck({ maxConcurrent: 32 });
-
-    // creates limited 'fetchMatchData()' by wrapping original with limiter
     const limitedFetchMatchData = limiter.wrap(this.fetchMatchData.bind(this));
 
-    // iterates through each match ID, calling 'limitedFetchMatchData()' on each
     for (const matchId of matchIds) {
       const characterDetails = await limitedFetchMatchData(matchId);
 
-      // formats JSON response (removing HTML, organizing)
+      // formats JSON response (removes HTML, organizes data )
       characterDetails.forEach((characterDetail: CharacterDetail) => {
-        // formats teamnamerich
         characterDetail.teamnamerich = characterDetail.teamnamerich.replace(
           /<[^>]+>/g,
           ""
         );
 
-        // RegEx: matchmaking_change & personal_change
         const overallRegex = /(-?\d{1,})(?=\s*\(<span)/;
         const changeRegex = /((\+|-)\d+)(?=<\/span)/;
 
@@ -205,15 +202,13 @@ export class WarmaneCrawler implements Crawler {
         (summary) => summary.matchId === matchId
       );
 
-      // combines fetched match data with corresponding 'matchSummary' object to create 'matchDetails' object
+      // combines fetched match data w/ corresponding 'matchSummary' object
       if (matchSummary) {
         const matchDetails: MatchDetails = {
           ...matchSummary,
           character_details: characterDetails,
         };
 
-        console.log(matchDetailsList, characterDetails); //*dev use
-        // creates array of 'matchDetails' objects
         matchDetailsList.push(matchDetails);
       }
     }
