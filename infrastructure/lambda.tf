0759ac89ba8eb4fab25263222e24e0b19fb9f338
Brian Simoni <brsimoni@gmail.com>
2023-04-17 23:40:58 -0400

    update

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -43,13 +43,15 @@ resource "aws_lambda_function" "warmane_analytics_api_v2_main_function" {
   architectures = ["arm64"]
 
   s3_bucket = "simoni-enterprises-artifacts"
-  s3_key    = "v2.zip"
+  s3_key    = "briansimoni/warmane-pvp-analytics-api/v2.zip"
+
+  # source_code_hash = filebase64sha256("v2.zip")
 
   runtime = "nodejs18.x"
   timeout = 10
   handler = "main.handler"
 
-  source_code_hash = data.archive_file.main_lambda_code.output_base64sha256
+  # source_code_hash = data.archive_file.main_lambda_code.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 }
