be5659b0d332110f0a32691b449de882e75f3484
Brian Simoni <brsimoni@gmail.com>
2023-04-23 20:28:48 -0400

    Add DNS

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -1,8 +1,7 @@
 resource "aws_api_gateway_rest_api" "rest_api" {
-  name = "warmane_pvp_analytics_v2_api"
-  #body = yamlencode(local.openapi_spec)
-  body = data.template_file.openapi_template.rendered
-
+  name              = "warmane_pvp_analytics_v2_api"
+  body              = data.template_file.openapi_template.rendered
+  put_rest_api_mode = "merge"
 }
 
 resource "aws_api_gateway_deployment" "main_deployment" {
@@ -22,3 +21,4 @@ resource "aws_api_gateway_stage" "main_stage" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   stage_name    = "main_stage"
 }
+
---
+++ infrastructure/dns.tf
@@ -0,0 +1,34 @@
+data "aws_route53_zone" "warmane_dot_dog" {
+  name         = "warmane.dog"
+  private_zone = false
+}
+
+resource "aws_api_gateway_domain_name" "api" {
+  domain_name              = "api.warmane.dog"
+  regional_certificate_arn = aws_acm_certificate.api.arn
+  security_policy          = "TLS_1_2"
+  depends_on               = [aws_acm_certificate_validation.api]
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_route53_record" "api" {
+  name    = aws_api_gateway_domain_name.api.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.warmane_dot_dog.zone_id
+
+  alias {
+    name                   = aws_api_gateway_domain_name.api.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
+resource "aws_api_gateway_base_path_mapping" "api" {
+  api_id      = aws_api_gateway_rest_api.rest_api.id
+  domain_name = aws_api_gateway_domain_name.api.id
+  stage_name  = aws_api_gateway_stage.main_stage.stage_name
+}
