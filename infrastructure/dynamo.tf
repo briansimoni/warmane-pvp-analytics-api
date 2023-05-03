7c3347fa10249faa9437e5bb5ba8fbe75da2c690
Brian Simoni <brsimoni@gmail.com>
2023-05-02 22:57:01 -0400

    Dynamo terraform (#22)
    * dynamooo
    
    * fmt
    
    * update

--- infrastructure/dynamo.tf
+++ infrastructure/dynamo.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "warmane_dynamo_table" {
-  name           = "${terraform.workspace}_warmane_matches"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "character"
-  range_key      = "document_key"
+  name         = "${terraform.workspace}_warmane_matches"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "character"
+  range_key    = "document_key"
   server_side_encryption {
     enabled = true
   }
