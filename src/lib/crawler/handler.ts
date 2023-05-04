96b773c059e74f6268d1d6c3b84be8fbacac67b5
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-05-04 19:38:27 -0400

    Created DynamoDB service class and integrated it into handler

--- src/lib/crawler/handler.ts
+++ src/lib/crawler/handler.ts
@@ -1,29 +1,45 @@
 import { Context, SQSEvent } from "aws-lambda";
-//* import { WarmaneCrawler } from "./crawler";
+import { WarmaneCrawler } from "./crawler";
+import DynamoService from "../../dynamoService";
 // import bodyParser from "koa-bodyparser";
+
+const dynamoService = new DynamoService("MatchData");
 
 export async function crawlerHandler(
   event: SQSEvent,
-  context: Context
-  //* warmaneCrawler: WarmaneCrawler
+  context: Context,
+  warmaneCrawler: WarmaneCrawler
 ) {
   console.log("sqsHandler");
   console.log(event);
   console.log(context);
 
-  //*   const character = event.Records[0].messageAttributes.character.stringValue || "";
-  //*   const realm = event.Records[0].messageAttributes.realm.stringValue || "";
-  //
-  //*   const matchSummaries = await warmaneCrawler.getMatchSummaries({
-  //*     character,
-  //*     realm,
-  //*   });
-  //
-  //*  const matchDetailsList = await warmaneCrawler.getMatchDetails({
-  //*    character,
-  //*    realm,
-  //*    matchSummaries,
-  //*  });
-}
+  const character =
+    event.Records[0].messageAttributes.character.stringValue || "";
+  const realm = event.Records[0].messageAttributes.realm.stringValue || "";
 
-//* = commented-out to pass PR checks, as `matchDetailsList` won't be used until DB implementation
+  const matchSummaries = await warmaneCrawler.getMatchSummaries({
+    character,
+    realm,
+  });
+
+  const matchDetailsList = await warmaneCrawler.getMatchDetails({
+    character,
+    realm,
+    matchSummaries,
+  });
+
+  for (const matchDetails of matchDetailsList) {
+    try {
+      await dynamoService.createItem(matchDetails);
+      console.log(
+        `Successfully stored match details for ${character} on ${realm}`
+      );
+    } catch (error) {
+      console.error(
+        `Error storing match details for ${character} on ${realm}:`,
+        error
+      );
+    }
+  }
+}
