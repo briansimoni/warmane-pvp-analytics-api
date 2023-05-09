842aae3d61128062a79c2461059e885c90cd0f23
Brian Simoni <brian.simoni@aura.com>
2023-05-09 13:25:09 -0400

    fix dependency

--- src/localServer.ts
+++ src/localServer.ts
@@ -2,6 +2,12 @@ import { api } from "./api";
 import { crawler } from "./crawler";
 import { logger } from "./lib/util/logger";
 
+/**
+ * This provides a way to run the Lambda code locally.
+ * Running a Koa server effectively simulates API Gateway + Lambda.
+ * With only a slight modification, you could run this on a real
+ * server or a container.
+ */
 if (process.env.AWS_EXECUTION_ENV === undefined) {
   api.listen(4000, () => {
     logger.info("api lambda listening on 4000");
