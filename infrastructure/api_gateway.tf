10242b5a86fdd8f29783d782b8ef1be9b68e413c
Brian Simoni <brian.simoni@aura.com>
2023-05-16 19:02:00 -0400

    change api gateway to proxy

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -1,24 +1,39 @@
-resource "aws_api_gateway_rest_api" "rest_api" {
+resource "aws_api_gateway_rest_api" "warmane_api_gateway" {
   name              = "${terraform.workspace}_warmane_pvp_analytics_v2_api"
   body              = data.template_file.openapi_template.rendered
   put_rest_api_mode = "merge"
 }
 
-resource "aws_api_gateway_deployment" "main_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+resource "aws_api_gateway_resource" "proxy_resource" {
+  rest_api_id = aws_api_gateway_rest_api.warmane_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.warmane_api_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
-  }
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.warmane_api_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
 
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_api_gateway_integration" "proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.warmane_api_gateway.id
+  resource_id             = aws_api_gateway_resource.proxy_resource.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.warmane_analytics_api_v2_main_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "default_deployment" {
+  depends_on  = [aws_api_gateway_integration.proxy_integration]
+  rest_api_id = aws_api_gateway_rest_api.warmane_api_gateway.id
 }
 
 resource "aws_api_gateway_stage" "main_stage" {
-  deployment_id = aws_api_gateway_deployment.main_deployment.id
+  deployment_id = aws_api_gateway_deployment.default_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  stage_name    = "main_stage"
+  stage_name    = terraform.workspace
 }
 
