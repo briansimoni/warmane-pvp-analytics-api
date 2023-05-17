106823b585b54b304edc1b1c1dacbb54ef534bc2
Brian Simoni <brian.simoni@aura.com>
2023-05-16 22:46:01 -0400

    move openapi yaml

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,20 +20,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-locals {
-  openapi_spec                               = yamldecode(file("./openapi.yaml"))
-  warmane_analytics_api_v2_main_function_arn = aws_lambda_function.warmane_analytics_api_v2_main_function.arn
-}
-
-data "template_file" "openapi_template" {
-  template = file("./openapi.yaml")
-
-
-  vars = {
-    warmane_analytics_api_v2_main_function_arn = "${aws_lambda_function.warmane_analytics_api_v2_main_function.arn}"
-    aws_region                                 = "us-east-1"
-  }
-
-}
-
