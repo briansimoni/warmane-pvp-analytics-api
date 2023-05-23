0a8e645d71ee385c91a4fb32df108c0c0dfdfd98
Brian Simoni <brsimoni@gmail.com>
2023-05-23 13:27:23 -0400

    update

--- infrastructure/dynamo.tf
+++ infrastructure/dynamo.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "warmane_dynamo_table" {
   name         = "${terraform.workspace}_warmane_matches"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "character"
+  hash_key     = "id"
   range_key    = "document_key"
   server_side_encryption {
     enabled = true
