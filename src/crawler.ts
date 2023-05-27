d29df52f537fc3583c285be7dfde125959f61710
Brian Simoni <brsimoni@gmail.com>
2023-05-27 11:58:19 -0400

    Crawler handler stuff (#31)
    * add crawler handler stuff
    
    * remove unused import
    
    * remove forEach
    
    * updates

--- src/crawler.ts
+++ src/crawler.ts
@@ -7,6 +7,13 @@ import {
 import koaBunyanLogger from "koa-bunyan-logger";
 import { logger } from "./lib/util/logger";
 import bodyParser from "koa-bodyparser";
+import {
+  GetCharacterRequestParams,
+  getCharacterSchema,
+} from "./api/validators";
+import { matchDetailsStore } from "./db/documentStoreV2";
+import { WarmaneCrawler } from "./lib/crawler/crawler";
+import createError from "http-errors";
 
 /**
  * This is the Koa object used by the crawler lambda
@@ -26,14 +33,41 @@ crawler.use(sqsMiddleware());
  * will run in response to receiving a message on the SQS queue.
  * In other words the API has requested that the crawler be invoked.
  */
-crawler.use((ctx) => {
-  logger.info(ctx);
-  logger.info(ctx.headers);
-  logger.info(ctx.body);
-  ctx.sqs.event.Records.forEach((record) => {
-    logger.info("this is the event!", record.body); // delete this line
-    // TODO: use JOI to validate incoming parameters
+crawler.use(async (ctx) => {
+  console.log(ctx.sqs.event.Records);
+  const validatedRequests = ctx.sqs.event.Records.map((r) => {
+    const validationResult = getCharacterSchema.validate(JSON.parse(r.body));
+    if (validationResult.error) {
+      logger.error(
+        `crawler recieved a bad request ${validationResult.error.message}`
+      );
+      throw createError.BadRequest(validationResult.error.message);
+    }
+    return validationResult.value;
   });
+  await handleCrawlerRequests(validatedRequests);
 });
 
+export async function handleCrawlerRequests(
+  requests: GetCharacterRequestParams[]
+) {
+  const handleRequest = async function (req: GetCharacterRequestParams) {
+    const crawler = new WarmaneCrawler();
+    logger.info(`crawling started for ${req.name} on ${req.realm}`);
+    const matchDetails = await crawler.fetchAllMatchDetails({
+      character: req.name,
+      realm: req.realm,
+    });
+    const databaseWrites = matchDetails.map((match) =>
+      matchDetailsStore.upsert(match)
+    );
+    logger.info(`crawling completed for ${req.name} on ${req.realm}`);
+    await Promise.all(databaseWrites);
+    logger.info(
+      `crawler results saved successfully for ${req.name} on ${req.realm}`
+    );
+  };
+
+  await Promise.all(requests.map(handleRequest));
+}
 export { crawler };
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,7 @@
 export type CharacterName = string;
 
-export type Realm = "Blackrock" | "Icecrown" | "Lordaeron";
+export const Realms = ["Blackrock", "Icecrown", "Lordaeron"] as const;
+
+export type Realm = (typeof Realms)[number];
 
 export type CharacterId = `${CharacterName}@${Realm}`;
