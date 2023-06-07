995e36a0602206f9af58057c820d116caf40ed2f
Brian Simoni <brsimoni@gmail.com>
2023-06-07 14:40:52 -0400

    Increase visibility timeout (#34)

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -3,7 +3,7 @@ resource "aws_sqs_queue" "crawl_queue" {
   max_message_size           = 2048
   message_retention_seconds  = 86400
   receive_wait_time_seconds  = 10
-  visibility_timeout_seconds = 180
+  visibility_timeout_seconds = 900
   sqs_managed_sse_enabled    = true
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.warmane_crawler_queue_dlq.arn
